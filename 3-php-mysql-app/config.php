c539669095dc5dffcf91e88707184523f2f3d95f
MostaphaOuahmane <87656171+MostaphaOuahmane@users.noreply.github.com>
2021-12-23 17:16:22 +0100

    fgrtjyyrryy

--- 3-php-mysql-app/config.php
+++ 3-php-mysql-app/config.php
@@ -1,13 +1,21 @@
 <?php
-
 $hostname = "localhost";
 $database_name = "ideasdb";
 $database_user = "mostapha";
-$database_user_password = "123";
+$database_user_password = "123"; 
 $dsn = "mysql:host=$hostname;dbname=$database_name";
 
-$options = array(
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 
+$options = array(
+    PDO::ATTR_ERRMODE => PDO ::ERRMODE_EXCEPTION,
+    // هنا نقوم بالقيام اتربيوت لاظهار الاخطاء في الجداول
 );
- ?>
+
+
+
+
+//هنا الاعلان عن قاعدة البيانات 
+
+
+
+?>
